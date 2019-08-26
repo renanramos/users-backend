4b9e19925fe05491d9ce859b2d44d95c67861e40
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-08-25 22:28:21 -0300

    Criado arquivo server.js e refatorado arquivo app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const mod = require('./modules/module')
-const bodyParse = require('body-parser')
-const express = require('express')
+const mod = require('./modules/module');
+const bodyParse = require('body-parser');
+const express = require('express');
 const app = express();
-
-const port = process.env.PORT || 5000;
+const userController = require('./controller/user.controller');
 
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true }))
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE")
@@ -15,89 +15,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/', (req, res) => res.send('App NodeJS MySQL'))
+app.use('/', (req, res) => {
+    res.status(200).json('Server is running!');
+});
+app.use('/api', userController);
 
-app.get('/api/users', (req, res) => {
-    mod.getAllUsers((data) => {
-        if (data == undefined) {
-            res.json(data)
-            res.status(200)
-        } else {
-            res.json(data)
-            res.status(200)
-        }
-    })
-})
-
-app.get('/api/users/:userId', (req, res) => {    
-    let id = req.params.userId
-    let response = {}    
-    if(id !== null && id !== undefined){
-        mod.getUserById(id, (data) => {
-            if(Object.entries(data).length > 0){
-                res.json(data)
-                res.status(200)
-            }else{
-                response.status = 404
-                response.message = 'Usuário não encontrado ou não existe.'
-                res.json(response)
-                res.status(404)
-            }
-        })
-    }else{
-        response.status = 404
-        response.message = 'Usuário não encontrado ou não existe.'
-        res.json(JSON.stringify(response))
-        res.status(404)
-    }
-})
-
-app.post('/api/users', (req, res) => {
-    let user = { name, lastname, age } = req.body
-    let response = {}
-
-    if (req.body === null || req.body === undefined) {
-        response = {
-            status: 400,
-            message: "Erro na requisição. Dados não informados"
-        }
-        res.json(JSON.stringify(response));
-        res.status(400);
-    } else {
-        mod.createUser(user, (response) =>{            
-            res.json(JSON.stringify(response));
-            res.status(200);
-        })
-    }
-})
-
-app.put('/api/users', (req, res) => {    
-    let user = req.body
-    if (Object.entries(user).length == 0) {
-        // TODO tratar retorno de erro
-    } else {
-        mod.updateUser(user, (response) => {
-            res.json(JSON.stringify(response))
-            res.status(200)
-        })
-    }
-})
-
-app.delete('/api/users/:id', (req, res) => {
-    let id = req.params.id
-    if(id > 0){
-        mod.deleteUser(id, (response) => {
-            res.json(JSON.stringify(response))
-            res.status(200)
-        })
-    }else{
-        let response = {
-            status: 404,
-            message: 'Usuário não existe ou não foi encontrado'
-        }
-        res.json(JSON.stringify(response))
-        res.status(404)
-    }
-})
-
-app.listen(port, () => console.log(`Server running on port ${port}`))
+module.exports = app;
